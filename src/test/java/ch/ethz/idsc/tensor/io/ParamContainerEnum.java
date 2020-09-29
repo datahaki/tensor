99a376c4682086fa93debf3c9b4f02c4cf9f7de5
Jan Hakenberg <jan.hakenberg@gmail.com>
2020-09-29 09:44:40 +0200

    tensor properties obsolete

--- src/test/java/ch/ethz/idsc/tensor/io/ParamContainerEnum.java
+++
@@ -1,9 +0,0 @@
-// code by jph
-package ch.ethz.idsc.tensor.io;
-
-import ch.ethz.idsc.tensor.mat.Pivots;
-
-public class ParamContainerEnum {
-  public Pivots pivots;
-  public NameString nameString;
-}
