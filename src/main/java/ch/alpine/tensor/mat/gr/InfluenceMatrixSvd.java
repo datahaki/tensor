f97b30958969b181eca2f6e9675f02f23ab7f527
datahaki <jan.hakenberg@gmail.com>
2021-10-09 11:36:47 +0200

    influence matrix svd

--- src/main/java/ch/alpine/tensor/mat/gr/InfluenceMatrixSvd.java
+++ src/main/java/ch/alpine/tensor/mat/gr/InfluenceMatrixSvd.java
@@ -2,19 +2,24 @@
 package ch.alpine.tensor.mat.gr;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import ch.alpine.tensor.Scalar;
 import ch.alpine.tensor.Tensor;
+import ch.alpine.tensor.mat.Tolerance;
 import ch.alpine.tensor.mat.pi.PseudoInverse;
 import ch.alpine.tensor.mat.sv.SingularValueDecomposition;
 
 /* package */ class InfluenceMatrixSvd extends InfluenceMatrixBase implements Serializable {
   private final Tensor design;
   private final SingularValueDecomposition svd;
-  /* matrix is computed on demand */
+  /* matrix computed on demand */
   private Tensor matrix;
+  /* matrix u computed on demand, svd.getU() with columns associated to non-zero singular value */
+  private Tensor u_;
 
   public InfluenceMatrixSvd(Tensor design) {
     this.design = design;
@@ -39,14 +44,18 @@ import ch.alpine.tensor.mat.sv.SingularValueDecomposition;
   }
 
   @Override // from InfluenceMatrix
-  public Tensor image(Tensor vector) {
-    Tensor u = svd.getU();
-    Tensor kron = Tensor.of(svd.values().stream() //
-        .map(Scalar.class::cast) //
-        .map(StaticHelper::unitize_chop)); // emulates v / v for v != 0
-    // TODO could still optimize further by extracting elements from rows in u
-    // Tensor U = Tensor.of(u.stream().map(kron::pmul)); // extract instead of pmul!
-    // return U.dot(vector.dot(U));
-    return u.dot(kron.pmul(vector.dot(u)));
+  public synchronized Tensor image(Tensor vector) {
+    return u_().dot(vector.dot(u_()));
+  }
+
+  private Tensor u_() {
+    if (Objects.isNull(u_)) {
+      Tensor values = svd.values();
+      List<Integer> list = IntStream.range(0, values.length()) //
+          .filter(index -> !Tolerance.CHOP.isZero(values.Get(index))) //
+          .boxed().collect(Collectors.toList());
+      u_ = Tensor.of(svd.getU().stream().map(row -> Tensor.of(list.stream().map(row::Get))));
+    }
+    return u_;
   }
 }
--- src/test/java/ch/alpine/tensor/mat/gr/StaticHelperTest.java
+++ src/test/java/ch/alpine/tensor/mat/gr/StaticHelperTest.java
@@ -5,11 +5,23 @@ import java.lang.reflect.Modifier;
 
 import ch.alpine.tensor.RealScalar;
 import ch.alpine.tensor.Scalar;
+import ch.alpine.tensor.mat.Tolerance;
 import ch.alpine.tensor.sca.Clips;
+import ch.alpine.tensor.sca.InvertUnlessZero;
 import ch.alpine.tensor.usr.AssertFail;
 import junit.framework.TestCase;
 
 public class StaticHelperTest extends TestCase {
+  private static final Scalar _0 = RealScalar.of(0.0);
+  private static final Scalar _1 = RealScalar.of(1.0);
+
+  /** @param scalar
+   * @return numerical 0 or 1
+   * @see InvertUnlessZero */
+  private static Scalar unitize_chop(Scalar scalar) {
+    return Tolerance.CHOP.isZero(scalar) ? _0 : _1;
+  }
+
   public void testRequireUnit() {
     Scalar scalar = RealScalar.of(1.0 + 1e-13);
     Scalar mapped = StaticHelper.requireUnit(scalar);
@@ -22,9 +34,9 @@ public class StaticHelperTest extends TestCase {
   }
 
   public void testUnitizeChop() {
-    assertEquals(StaticHelper.unitize_chop(RealScalar.of(1e-13)), RealScalar.ZERO);
-    assertEquals(StaticHelper.unitize_chop(RealScalar.of(1e-11)), RealScalar.ONE);
-    assertEquals(StaticHelper.unitize_chop(RealScalar.of(123)), RealScalar.ONE);
+    assertEquals(StaticHelperTest.unitize_chop(RealScalar.of(1e-13)), RealScalar.ZERO);
+    assertEquals(StaticHelperTest.unitize_chop(RealScalar.of(1e-11)), RealScalar.ONE);
+    assertEquals(StaticHelperTest.unitize_chop(RealScalar.of(123)), RealScalar.ONE);
   }
 
   public void testPackageVisibility() {
