be56330be59ef39df227d553c2cd5aa106f8a04b
datahaki <jan.hakenberg@gmail.com>
2022-06-21 13:43:32 +0200

    unary ops @JoelGaechter

--- src/main/java/ch/alpine/tensor/api/ScalarUnaryOperator.java
+++ src/main/java/ch/alpine/tensor/api/ScalarUnaryOperator.java
@@ -1,7 +1,8 @@
-// code by jph
+// code by jph, gjoel
 package ch.alpine.tensor.api;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.function.UnaryOperator;
 
 import ch.alpine.tensor.Scalar;
@@ -9,5 +10,18 @@ import ch.alpine.tensor.Scalar;
 /** interface for serializable functions that map a {@link Scalar} to another {@link Scalar} */
 @FunctionalInterface
 public interface ScalarUnaryOperator extends UnaryOperator<Scalar>, Serializable {
-  // ---
+  // TODO TENSOR DOC
+  default ScalarUnaryOperator compose(ScalarUnaryOperator before) {
+    Objects.requireNonNull(before);
+    return s -> apply(before.apply(s));
+  }
+
+  default ScalarUnaryOperator andThen(ScalarUnaryOperator after) {
+    Objects.requireNonNull(after);
+    return s -> after.apply(apply(s));
+  }
+
+  static ScalarUnaryOperator identity() {
+    return s -> s;
+  }
 }
--- src/main/java/ch/alpine/tensor/api/TensorUnaryOperator.java
+++ src/main/java/ch/alpine/tensor/api/TensorUnaryOperator.java
@@ -1,7 +1,8 @@
-// code by jph
+// code by jph, gjoel
 package ch.alpine.tensor.api;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.function.UnaryOperator;
 
 import ch.alpine.tensor.Tensor;
@@ -9,5 +10,18 @@ import ch.alpine.tensor.Tensor;
 /** interface for serializable functions that map a {@link Tensor} to another {@link Tensor} */
 @FunctionalInterface
 public interface TensorUnaryOperator extends UnaryOperator<Tensor>, Serializable {
-  // ---
+  // TODO TENSOR DOC
+  default TensorUnaryOperator compose(TensorUnaryOperator before) {
+    Objects.requireNonNull(before);
+    return s -> apply(before.apply(s));
+  }
+
+  default TensorUnaryOperator andThen(TensorUnaryOperator after) {
+    Objects.requireNonNull(after);
+    return s -> after.apply(apply(s));
+  }
+
+  static TensorUnaryOperator identity() {
+    return s -> s;
+  }
 }
