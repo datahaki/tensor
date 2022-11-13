faf549b43a86386bdb463e1726650c4160dc539a
datahaki <jan.hakenberg@gmail.com>
2022-11-13 11:04:16 +0100

    xtrogram array

--- src/main/java/ch/alpine/tensor/fft/SpectrogramArray.java
+++ src/main/java/ch/alpine/tensor/fft/SpectrogramArray.java
@@ -3,11 +3,8 @@ package ch.alpine.tensor.fft;
 
 import ch.alpine.tensor.Scalar;
 import ch.alpine.tensor.Tensor;
-import ch.alpine.tensor.Tensors;
-import ch.alpine.tensor.Unprotect;
 import ch.alpine.tensor.api.ScalarUnaryOperator;
 import ch.alpine.tensor.api.TensorUnaryOperator;
-import ch.alpine.tensor.sca.Abs;
 import ch.alpine.tensor.sca.win.DirichletWindow;
 import ch.alpine.tensor.sca.win.HannWindow;
 import ch.alpine.tensor.sca.win.WindowFunctions;
@@ -22,8 +19,7 @@ public enum SpectrogramArray {
    * @param window for instance {@link DirichletWindow#FUNCTION}
    * @return */
   public static Tensor of(Tensor vector, ScalarUnaryOperator window) {
-    int windowLength = StaticHelper.default_windowLength(vector.length());
-    return of(windowLength, StaticHelper.default_offset(windowLength), window).apply(vector);
+    return XtrogramArray.SPECTRO.of(vector, window);
   }
 
   /** Mathematica default
@@ -32,7 +28,7 @@ public enum SpectrogramArray {
    * @return
    * @throws Exception if input is not a vector */
   public static Tensor of(Tensor vector) {
-    return of(vector, DirichletWindow.FUNCTION);
+    return XtrogramArray.SPECTRO.of(vector);
   }
 
   /** @param vector
@@ -40,9 +36,7 @@ public enum SpectrogramArray {
    * @return truncated and transposed spectrogram array for visualization
    * @throws Exception if input is not a vector */
   public static Tensor half_abs(Tensor vector, ScalarUnaryOperator window) {
-    Tensor tensor = of(vector, window);
-    int half = Unprotect.dimension1Hint(tensor) / 2;
-    return Tensors.vector(i -> tensor.get(Tensor.ALL, half - i - 1).map(Abs.FUNCTION), half);
+    return XtrogramArray.SPECTRO.half_abs(vector, window);
   }
 
   // ---
@@ -51,24 +45,14 @@ public enum SpectrogramArray {
    * @param window for instance {@link DirichletWindow#FUNCTION}
    * @return */
   public static TensorUnaryOperator of(int windowLength, int offset, ScalarUnaryOperator window) {
-    return new BasetrogramArray(windowLength, offset, window) {
-      @Override
-      protected Tensor processBlock(Tensor vector) {
-        return Fourier.of(vector);
-      }
-
-      @Override
-      protected String name() {
-        return "SpectrogramArray";
-      }
-    };
+    return XtrogramArray.SPECTRO.of(windowLength, offset, window);
   }
 
   /** @param windowLength
    * @param offset positive and not greater than windowLength
    * @return */
   public static TensorUnaryOperator of(int windowLength, int offset) {
-    return of(windowLength, offset, DirichletWindow.FUNCTION);
+    return XtrogramArray.SPECTRO.of(windowLength, offset);
   }
 
   /** @param windowDuration
@@ -78,7 +62,7 @@ public enum SpectrogramArray {
    * @return */
   public static TensorUnaryOperator of( //
       Scalar windowDuration, Scalar samplingFrequency, int offset, ScalarUnaryOperator window) {
-    return of(StaticHelper.windowLength(windowDuration, samplingFrequency), offset, window);
+    return XtrogramArray.SPECTRO.of(windowDuration, samplingFrequency, offset, window);
   }
 
   /** @param windowDuration
@@ -87,7 +71,6 @@ public enum SpectrogramArray {
    * @return spectrogram operator with default offset */
   public static TensorUnaryOperator of( //
       Scalar windowDuration, Scalar samplingFrequency, ScalarUnaryOperator window) {
-    int windowLength = StaticHelper.windowLength(windowDuration, samplingFrequency);
-    return of(windowLength, StaticHelper.default_offset(windowLength), window);
+    return XtrogramArray.SPECTRO.of(windowDuration, samplingFrequency, window);
   }
 }
---
+++ src/main/java/ch/alpine/tensor/fft/XtrogramArray.java
@@ -0,0 +1,115 @@
+// code by ob, jph
+package ch.alpine.tensor.fft;
+
+import ch.alpine.tensor.Scalar;
+import ch.alpine.tensor.Tensor;
+import ch.alpine.tensor.Tensors;
+import ch.alpine.tensor.Unprotect;
+import ch.alpine.tensor.api.ScalarUnaryOperator;
+import ch.alpine.tensor.api.TensorUnaryOperator;
+import ch.alpine.tensor.sca.Abs;
+import ch.alpine.tensor.sca.win.DirichletWindow;
+import ch.alpine.tensor.sca.win.HannWindow;
+import ch.alpine.tensor.sca.win.WindowFunctions;
+
+/** inspired by
+ * <a href="https://reference.wolfram.com/language/ref/SpectrogramArray.html">SpectrogramArray</a>
+ * 
+ * @see WindowFunctions */
+public enum XtrogramArray {
+  SPECTRO {
+    @Override
+    public TensorUnaryOperator of(int windowLength, int offset, ScalarUnaryOperator window) {
+      return new BasetrogramArray(windowLength, offset, window) {
+        @Override
+        protected Tensor processBlock(Tensor vector) {
+          return Fourier.of(vector);
+        }
+
+        @Override
+        protected String name() {
+          return "SpectrogramArray";
+        }
+      };
+    }
+  },
+  CEPSTRO {
+    @Override
+    public TensorUnaryOperator of(int windowLength, int offset, ScalarUnaryOperator window) {
+      return new BasetrogramArray(windowLength, offset, window) {
+        @Override
+        protected Tensor processBlock(Tensor vector) {
+          return CepstrumArray.REAL.apply(vector);
+        }
+
+        @Override
+        protected String name() {
+          return "CepstrogramArray";
+        }
+      };
+    }
+  }
+  //
+  ;
+
+  /** @param vector
+   * @param window for instance {@link DirichletWindow#FUNCTION}
+   * @return */
+  public final Tensor of(Tensor vector, ScalarUnaryOperator window) {
+    int windowLength = StaticHelper.default_windowLength(vector.length());
+    return of(windowLength, StaticHelper.default_offset(windowLength), window).apply(vector);
+  }
+
+  /** Mathematica default
+   * 
+   * @param vector
+   * @return
+   * @throws Exception if input is not a vector */
+  public final Tensor of(Tensor vector) {
+    return of(vector, DirichletWindow.FUNCTION);
+  }
+
+  /** @param vector
+   * @param window for instance {@link HannWindow#FUNCTION}
+   * @return truncated and transposed spectrogram array for visualization
+   * @throws Exception if input is not a vector */
+  public final Tensor half_abs(Tensor vector, ScalarUnaryOperator window) {
+    Tensor tensor = of(vector, window);
+    int half = Unprotect.dimension1Hint(tensor) / 2;
+    return Tensors.vector(i -> tensor.get(Tensor.ALL, half - i - 1).map(Abs.FUNCTION), half);
+  }
+
+  /** @param windowLength
+   * @param offset positive and not greater than windowLength
+   * @return */
+  public final TensorUnaryOperator of(int windowLength, int offset) {
+    return of(windowLength, offset, DirichletWindow.FUNCTION);
+  }
+
+  /** @param windowDuration
+   * @param samplingFrequency
+   * @param offset positive
+   * @param window for instance {@link DirichletWindow#FUNCTION}
+   * @return */
+  public final TensorUnaryOperator of( //
+      Scalar windowDuration, Scalar samplingFrequency, int offset, ScalarUnaryOperator window) {
+    return of(StaticHelper.windowLength(windowDuration, samplingFrequency), offset, window);
+  }
+
+  /** @param windowDuration
+   * @param samplingFrequency
+   * @param window for instance {@link DirichletWindow#FUNCTION}
+   * @return spectrogram operator with default offset */
+  public final TensorUnaryOperator of( //
+      Scalar windowDuration, Scalar samplingFrequency, ScalarUnaryOperator window) {
+    int windowLength = StaticHelper.windowLength(windowDuration, samplingFrequency);
+    return of(windowLength, StaticHelper.default_offset(windowLength), window);
+  }
+
+  // ---
+  /** @param windowLength
+   * @param offset
+   * @param window for instance {@link DirichletWindow#FUNCTION}
+   * @return */
+  public abstract TensorUnaryOperator of(int windowLength, int offset, ScalarUnaryOperator window);
+}
