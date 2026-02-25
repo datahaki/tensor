6b09a856e1cae56413118b230f36601421264132
datahaki <jan.hakenberg@gmail.com>
2026-02-25 06:58:54 +0100

    package ext ref obsolete

--- src/main/java/ch/alpine/tensor/ext/ref/ClassVisitor.java
+++
@@ -1,9 +0,0 @@
-// code by lcm
-package ch.alpine.tensor.ext.ref;
-
-@FunctionalInterface
-public interface ClassVisitor {
-  /** @param jarfile
-   * @param cls */
-  void accept(String jarfile, Class<?> cls);
-}
